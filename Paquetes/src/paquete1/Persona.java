ad0b0e87ecfd43972e3b3471d34390db62df674b
Alumno <Alumno@ws204-05>
2019-02-06 18:01:41 -0800

    Version 1

--- Paquetes/src/paquete1/Persona.java
+++ Paquetes/src/paquete1/Persona.java
@@ -21,6 +21,7 @@ public class Persona {
 	public void setNombre(String nombre) {
 		this.nombre = nombre;
 	}
+	
 	@Override
 	public String toString() {
 		return "Persona [edad=" + edad + ", nombre=" + nombre + "]";
