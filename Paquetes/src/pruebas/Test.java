a769fe633fc1f9c46fd34a4191104ba02e03aeb7
memo24813 <35637786+memo24813@users.noreply.github.com>
2019-02-06 18:09:48 -0800

    cambios prueba

--- Paquetes/src/pruebas/Test.java
+++ Paquetes/src/pruebas/Test.java
@@ -7,9 +7,10 @@ public class Test {
 		
 		paquete1.Persona p= new paquete1.Persona(21,"Guillermo");
 		paquete2.Persona m= new paquete2.Persona(21,"Diana","Femenino");
-		
+		System.out.println("Inicio de impresion: ");
 		System.out.println(p.toString());
 		System.out.println(m.toString());
+		System.out.println("Fin!....");
 	}
 
 }
