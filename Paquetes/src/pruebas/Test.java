ab4fa2c9b9000553867b86846d0499d769aab428
Alumno <Alumno@ws204-05>
2019-02-06 18:11:59 -0800

    Cambio Local

--- Paquetes/src/pruebas/Test.java
+++ Paquetes/src/pruebas/Test.java
@@ -1,5 +1,7 @@
 package pruebas;
 
+import paquete1.Persona;
+
 public class Test {
 
 	public static void main(String[] args) {
@@ -7,9 +9,10 @@ public class Test {
 		
 		paquete1.Persona p= new paquete1.Persona(21,"Guillermo");
 		paquete2.Persona m= new paquete2.Persona(21,"Diana","Femenino");
-		
+		paquete1.Persona n= new paquete1.Persona(21,"pepe");
 		System.out.println(p.toString());
 		System.out.println(m.toString());
+		System.out.println(n.toString());
 	}
 
 }
