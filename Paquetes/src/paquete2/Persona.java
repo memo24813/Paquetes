24f25438946b5701fa1ae51c07a018ca1d6cecef
Alumno <Alumno@ws204-05>
2019-02-06 17:52:00 -0800

    Version 1.0

---
+++ Paquetes/src/paquete2/Persona.java
@@ -0,0 +1,41 @@
+package paquete2;
+
+public class Persona {
+
+	private int edad;
+	private String nombre;
+	private String genero;
+	
+	public Persona(int edad, String nombre, String genero) {
+		super();
+		this.edad = edad;
+		this.nombre = nombre;
+		this.genero = genero;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+	
+	
+}
---
+++ Paquetes/src/pruebas/Test.java
@@ -0,0 +1,15 @@
+package pruebas;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		paquete1.Persona p= new paquete1.Persona(21,"Guillermo");
+		paquete2.Persona m= new paquete2.Persona(21,"Diana","Femenino");
+		
+		System.out.println(p.toString());
+		System.out.println(m.toString());
+	}
+
+}
